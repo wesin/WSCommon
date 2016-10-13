7a56540c3310ba325df2305adf3f752b960a5542
wesin <wesinlove@hotmail.com>
2016-10-13 11:15:38 +0800

    Initial Commit

---
+++ WSCommon/WSCommon.h
@@ -0,0 +1,19 @@
+//
+//  WSCommon.h
+//  WSCommon
+//
+//  Created by 何文新 on 2016/10/13.
+//  Copyright © 2016年 wesin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WSCommon.
+FOUNDATION_EXPORT double WSCommonVersionNumber;
+
+//! Project version string for WSCommon.
+FOUNDATION_EXPORT const unsigned char WSCommonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WSCommon/PublicHeader.h>
+
+
